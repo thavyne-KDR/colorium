dce93a25858c9cd41dbbd79eb2954f174923a2a0
anafariass <livia.oliveira1.only@gmail.com>
2026-01-22 16:42:21 -0300

    chore: resolve more  merge conflicts with origin/main

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,4 @@
 require('dotenv').config()
-<<<<<<< HEAD
-const fastify = require('fastify')({ logger: true })
-const authRoutes = require('./routes/auth')
-
-// JWT
-fastify.register(require('@fastify/jwt'), {
-  secret: process.env.JWT_SECRET
-})
-
-fastify.decorate('authenticate', async function (request, reply) {
-  try {
-    await request.jwtVerify()
-  } catch (err) {
-    reply.code(401).send({ message: 'Token inválido ou ausente' })
-  }
-})
-
-// 🔗 PLUGIN DO BANCO
-fastify.register(require('./plugins/db'))
-
-// ROTAS
-fastify.register(authRoutes)
-
-const start = async () => {
-  try {
-    await fastify.listen({ port: 3333 })
-    console.log('Servidor rodando em http://localhost:3333')
-=======
 
 const fastify = require('fastify')({ logger: true })
 const cors = require('@fastify/cors')
@@ -45,7 +17,6 @@ async function start() {
 
     await fastify.listen({ port: 3333 })
     console.log('🚀 Servidor rodando em http://localhost:3333')
->>>>>>> 8f0e487d5586f9b375d105ffa687ffbbf6754823
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
